20d16448927b5b8a39ff017cf3f5cd6b917dc9c3
CodeBrew28 <arshiam2@illinois.edu>
2018-03-19 17:05:29 -0700

    file schema

--- models/file.js
+++ models/file.js
@@ -1,11 +1,11 @@
 const { Schema, SchemaTypes } = require('mongoose')
 const timestamps = require('mongoose-timestamp')
 
-const ScriptSchema = new Schema({
+const FileSchema = new Schema({
     key: { type: String, required: true, unique: true },
     url: { type: String, required: true, unique: true },
   })
   
 
-ScriptSchema.plugin(timestamps)
-module.exports = m => m.model('Script', ScriptSchema)
+FileSchema.plugin(timestamps)
+module.exports = m => m.model('File', FileSchema)
